60d9f5cec6c52645ce5bebf3fa6fc1185c927e68
Denis <ldu2601@gmail.com>
2022-06-01 15:28:19 +0300

    fix

--- sources/js/app/gutenberg.js
+++ sources/js/app/gutenberg.js
@@ -1,5 +1,5 @@
-import {gsap} from "gsap/dist/gsap";
-import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
+import {gsap} from "./gsap/gsap";
+import {ScrollTrigger} from "./gsap/ScrollTrigger";
 import {isjQuery} from "./functions";
 
 gsap.registerPlugin(ScrollTrigger);
--- sources/js/script.js
+++ sources/js/script.js
@@ -1,7 +1,7 @@
 import './app/gutenberg';
 import Swiper from 'swiper/swiper-bundle';
-import {gsap} from "gsap/dist/gsap";
-import {ScrollTrigger} from "gsap/dist/ScrollTrigger";
+import {gsap} from "./app/gsap/gsap";
+import {ScrollTrigger} from "./app/gsap/ScrollTrigger";
 import {isEven, isjQuery, Coordinates} from "./app/functions";
 
 gsap.registerPlugin(ScrollTrigger);
